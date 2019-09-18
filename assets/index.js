5c2ac87e4c2f9bb6264e8b395759ef189b75d2d8
Ismael López <lc.ismael@hotmail.com>
2019-09-17 22:37:04 -0500

    initial commit

---
+++ assets/index.js
@@ -0,0 +1,17 @@
+import MediaPlayer from './MediaPlayer.js'
+import AutoPlay from './plugins/AutoPlay.js'
+
+const video = document.querySelector('video');
+const buttonPlay = document.querySelector('#btnPlay');
+const buttonMute = document.querySelector('#btnMute');
+
+const player = new MediaPlayer({ 
+	el: video,
+	plugins: [
+		// new AutoPlay()
+	]
+ });
+
+buttonPlay.onclick = () => player.togglePlay();
+buttonMute.onclick = () => player.toggleMute();
+// button.onclick = () => video.play();
